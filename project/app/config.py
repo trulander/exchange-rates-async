9a844fb76ff190f5e00bc5fb039e1c520764612e
Alexander <trulander@mail.ru>
2021-10-13 20:00:37 +0500

    Changed reat api urls

--- project/app/config.py
+++ project/app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     celery_backend: str = 'redis://localhost:6379/1'
     celery_default_task_time_interval: float = 15.0
 
-    url_to_endpoint_for_worker: str = 'http://localhost:8000/latest_rate'
+    url_to_endpoint_for_worker: str = 'http://localhost:8000/api/ratecurrency/latest/'
 
     # service coinmarketcap.com
     url_api_foreign_service_rates: str = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
--- project/app/main.py
+++ project/app/main.py
@@ -7,17 +7,17 @@ from app.dataaccess import DataAccess
 app = FastAPI()
 service_request = lambda: RequestCurrencyService(data_access=DataAccess())
 
-@app.get("/last_rate", response_model=CurrencyRate)
-async def get_last_rate(service: RequestCurrencyService = Depends(service_request)):
-    result = await service.get_last_rate()
-    return result
-
-@app.get("/list_rates", response_model=list[CurrencyRate])
+@app.get("/api/ratecurrency/", response_model=list[CurrencyRate])
 async def get_list_rates(service: RequestCurrencyService = Depends(service_request)):
     result = await service.get_list_rates()
     return result
 
-@app.get("/latest_rate", response_model=CurrencyRateCreate)
+@app.get("/api/ratecurrency/last/", response_model=CurrencyRate)
+async def get_last_rate(service: RequestCurrencyService = Depends(service_request)):
+    result = await service.get_last_rate()
+    return result
+
+@app.get("/api/ratecurrency/latest/", response_model=CurrencyRateCreate)
 async def get_latest_rate(service: RequestCurrencyService = Depends(service_request)):
     result = await service.get_latest_rate(1)
     return result
